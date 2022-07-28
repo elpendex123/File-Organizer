80f330383f555abec30e6cc37bf777c1b0defb88
elpendex123 <enrique.coello@gmail.com>
2022-07-27 21:24:54 -0400

    Transferred code from other repo to new repo

---
+++ src/test/java/com/example/fileorganizer/fileorganizer/FileOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileorganizer.fileorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
