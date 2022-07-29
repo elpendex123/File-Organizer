36d10f8c0f16fcfd9e03b179e74250db7c0e52e6
elpendex123 <enrique.coello@gmail.com>
2022-07-29 00:09:28 -0400

    Created a shell script to initialize the downloads directory with dummy files

--- create_files.sh
+++ create_files.sh
@@ -4,5 +4,17 @@ clear;
 
 echo "Creating files in the Downloads directory";
 
+base_directory=$(cd $(dirname $0) && pwd)
+properties_file=${base_directory}/src/main/resources/application.properties
+
+echo "BASE DIRECTORY: ${base_directory}"
+
+source ${properties_file}
+
+echo "Downloads directory: ${DOWNLOADS_DIRECTORY}"
+
+cd ${DOWNLOADS_DIRECTORY}
+touch file.txt file.mp3 file.mp4 file.jpg file.png file.rtf file.wav file.mov
+
 exit;
 
